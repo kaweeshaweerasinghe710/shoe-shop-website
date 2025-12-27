7d28586c12a5aefbaa01f2b5e24f86e00a946650
kaweesha weerasinghe <weerasinghamkh.23@uom.lk>
2025-12-27 11:18:42 +0530

    Refactor server.js; remove serverless integration, enhance route organization, and improve error handling for MongoDB connection.

--- api/index.js
+++
@@ -1,5 +0,0 @@
-
-const serverless = require('serverless-http');
-const app = require('../backend/server'); // path from api/index.js to backend/server.js
-
-module.exports.handler = serverless(app);
